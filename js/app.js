6dadf34d25d95f17676d30d7a9530965bfe6aaac
MiguelTamayo <34068757+MiguelTamayo@users.noreply.github.com>
2019-06-10 18:50:39 -0500

    added angularjs function for symbol input field

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 angular.module('profitCalculator',[])
 .controller('profitController',function($scope){
 	$scope.result = "test";
+	$scope.search = function(){
+		console.log("searching!");
+	}
+
 });
