07e65706cecb9f6c198a7ca2bc73b25e2ab22430
MiguelTamayo <34068757+MiguelTamayo@users.noreply.github.com>
2019-06-11 16:29:29 -0500

    integrated iex api v1

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
-angular.module('profitCalculator',[])
-.controller('profitController',function($scope){
+angular.module("profitCalculator",[])
+.controller("profitController",function($scope, $http){
 	$scope.result = "test";
+	$scope.stockSymbol = "";
+	$scope.currentPrice = "";
+
 	$scope.search = function(){
-		console.log("searching!");
+		console.log("searching for "+$scope.symbol+"!");
+
+		$http({method: "GET", url: "https://api.iextrading.com/1.0/stock/"+$scope.symbol+"/chart/1d"})
+		.then(function successCallback(response){
+			$scope.stockSymbol = $scope.symbol;
+			$scope.currentPrice = response.data[response.data.length-1].close;
+			console.log(response);
+		}, function errorCallback(response){
+			console.log("there was an error");
+		});
 	}
 
 });
