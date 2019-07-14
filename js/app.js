7112268f32436c981106c8c552e2b86ac013f071
MiguelTamayo <34068757+MiguelTamayo@users.noreply.github.com>
2019-07-14 00:59:08 -0500

    added arrow key controls for search bar results

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var application = angular.module("profitCalculator", ['zingchart-angularjs', 'ap
 application.controller("profitController", function($scope, $http, $filter, apiData, profitCalculator) {
 
     var symbolList;
+    var highlightedResultIndex = 0;
 
     //API call for symbols available
     apiData.getSymbolList().then(function successCallback (response){
@@ -14,6 +15,7 @@ application.controller("profitController", function($scope, $http, $filter, apiD
 
     $scope.autoComplete = function() {
     	$scope.results = [];
+    	console.log("symbol:",$scope.symbol);
 
     	//check if symbolList is defined and that search entry is not empty
     	if((symbolList != undefined) && ($scope.symbol != "")){
@@ -23,7 +25,9 @@ application.controller("profitController", function($scope, $http, $filter, apiD
 	    	//filter symbolList
 	    	for (var i = 0, len = symbolList.length; i < len; i++) {
 	    		if(patt.test(symbolList[i].symbol)){
-	    			$scope.results.push(symbolList[i]);
+	    			var result = symbolList[i];
+	    			result.class = "";
+	    			$scope.results.push(result);
 	    		}
 	    	}
 
@@ -31,21 +35,26 @@ application.controller("profitController", function($scope, $http, $filter, apiD
 	    	if($scope.results.length == 0){
 	    		$scope.results.push({symbol:"No Results found",name:"try another symbol"});
 	    	}
+
+	    	//highlight first search result
+	    	highlightedResultIndex = 0;
+    		$scope.results[highlightedResultIndex].class = "highlighted";
+
     	}else{
-    		console.log("symbols undefined!");
+    		console.log("symbols undefined! or symbol is empty");
     	}
     };
 
     $scope.search = function() {
         console.log("searching for " + $scope.symbol + "!");
-        var range = "Today"; //TEMP
+        var range = "5D"; //TEMP
 
         //API chart data call
         apiData.getSymbolChart($scope.symbol, range).then(function successCallback(response) {
             console.log("success, got a response!");
             var data = response.data;
             //check to see if response data is not empty
-            if(data != []) {
+            if(data.length != 0) {
                 var calculation = profitCalculator.getCalculation(data);
                 $scope.chartJson = calculation.chartJSON;
                 $scope.trades = calculation.trades;
@@ -59,10 +68,36 @@ application.controller("profitController", function($scope, $http, $filter, apiD
     };
 
     $scope.searchBarKeyInput = function(eventKey){
+    	console.log(eventKey);
     	//check if enter key has been pressed
     	if(eventKey.keyCode == 13){
+    		//set symbol to highlighted result
+    		$scope.symbol = $scope.results[highlightedResultIndex].symbol;
+    		//clear search results
+    		$scope.results = [];
     		$scope.search();
-    	}else{
+
+    	}
+    	//check if arrow keys are selected
+    	//up or left arrow keys
+    	else if((eventKey.keyCode == 37) || (eventKey.keyCode == 38)){
+    		//prevent going into negative index
+    		if(highlightedResultIndex >= 1){
+    			$scope.results[highlightedResultIndex].class = "";
+    			highlightedResultIndex -= 1;
+    			$scope.results[highlightedResultIndex].class = "highlighted";
+    		}
+    	}
+    	//down or right arrow keys
+    	else if((eventKey.keyCode == 39) || (eventKey.keyCode == 40)){
+    		//prevent going out of bounds
+    		if(highlightedResultIndex < $scope.results.length-1){
+    			$scope.results[highlightedResultIndex].class = "";
+    			highlightedResultIndex += 1;
+    			$scope.results[highlightedResultIndex].class = "highlighted";
+    		}
+    	}
+    	else{
     		//autocomplete results
     		$scope.autoComplete();
     	}
